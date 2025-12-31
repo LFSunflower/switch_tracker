efca886496974ffe1aadeb4a6a5931f5bf10e2d3
Giulia Grossi <giuliaggfatec@gmail.com>
2025-12-31 00:14:22 -0300

    a

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.switch_tracker"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = "29.0.14206865"
 
     compileOptions {
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.switch_tracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
